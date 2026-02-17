53480c09584e2a0bf6eed602fee1ce4c556a5285
Aleks-Ti <mti61ro@yandex.ru>
2026-02-17 22:56:29 +0300

    fix fmt

--- src/main.rs
+++ src/main.rs
@@ -57,11 +57,15 @@ fn main() {
 
     let args = std::env::args().collect::<Vec<_>>();
     let filename = args[1].clone();
-    println!("Trying opening file '{}' from directory '{}'", filename, std::env::current_dir().unwrap().to_string_lossy());
-    let file: std::rc::Rc<std::cell::RefCell<Box<dyn analysis::MyReader>>> = std::rc::Rc::new(std::cell::RefCell::new(Box::new(std::fs::File::open(filename).unwrap())));
+    println!(
+        "Trying opening file '{}' from directory '{}'",
+        filename,
+        std::env::current_dir().unwrap().to_string_lossy()
+    );
+    let file: std::rc::Rc<std::cell::RefCell<Box<dyn analysis::MyReader>>> =
+        std::rc::Rc::new(std::cell::RefCell::new(Box::new(std::fs::File::open(filename).unwrap())));
 
     let logs = analysis::read_log(file.clone(), analysis::READ_MODE_ALL, vec![]);
     println!("got logs:");
     logs.iter().for_each(|parsed| println!("  {:?}", parsed));
 }
-
