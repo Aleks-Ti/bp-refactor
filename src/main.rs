6259fe64562a5b120f67290307ec8d79aa59020e
Aleks-Ti <mti61ro@yandex.ru>
2026-02-18 01:45:46 +0300

    mode constants::исправлены константы для представление режима чтения логов, заменены константы на enum ReadMode

--- src/main.rs
+++ src/main.rs
@@ -65,7 +65,7 @@ fn main() {
     let file: std::rc::Rc<std::cell::RefCell<Box<dyn analysis::MyReader>>> =
         std::rc::Rc::new(std::cell::RefCell::new(Box::new(std::fs::File::open(filename).unwrap())));
 
-    let logs = analysis::read_log(file.clone(), analysis::READ_MODE_ALL, vec![]);
+    let logs = analysis::read_log(file.clone(), analysis::ReadMode::All, vec![]);
     println!("got logs:");
     logs.iter().for_each(|parsed| println!("  {:?}", parsed));
 }
